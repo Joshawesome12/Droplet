c71770c873d73c540663d7620811a06d587bb8bc
joshuapawlik <joshawesome12@yahoo.com>
2018-12-19 14:37:22 -0600

    Selects Random location/ Centers items

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      locations:["Junk Junction", "The Block", "Haunted Hills", "Pleasant Park", "Lazy Links", "Tomato Temple", "Wailing Woods", "Loot Lake", "Snobby Shores", "Tilted Towers", "Dusty Divot",
+       "Lonely Lodge", "Retail Row", "Shifty Shafts", "Salty Springs", "Frosty Flights", "Fatal Fields", "Paradise Palms", "Happy Hamlet", "Lucky Landing"],
+       drop: "Click Roll!"
     }
   }
 
@@ -12,16 +14,30 @@ class App extends Component {
   // Create list of locations
   // Gather images of locations
   // Add "Roll" button
-  
+
+  selectRandom = () => {
+    var num = Math.floor(Math.random() * 20);
+    // console.log(num)
+    var location = this.state.locations[num]
+    this.setState({drop: location})
+  }
+
 
   render() {
     return (
       <div>
         <h1 className="welcome">Welcome to Droplet</h1>
-        <img className="react-img" src={require('./resources/react.png')} alt='react'/>
+        <div className = "stuff">
+          <h2>{this.state.drop}</h2>
+          <button onClick={this.selectRandom}>Roll</button>
+        </div>
       </div>
     )
   }
 }
 
 export default App
+
+
+
+// <img className="react-img" src={require('./resources/react.png')} alt='react'/>
