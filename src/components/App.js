377d86f440b9d28b8f91261d3a94bf6b8b05387b
joshuapawlik <joshawesome12@yahoo.com>
2018-12-18 22:22:32 -0600

    Jumpstart with my react boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1 className="welcome">Welcome to Droplet</h1>
+        <img className="react-img" src={require('./resources/react.png')} alt='react'/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ webpack.common.js
@@ -0,0 +1,49 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js'
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: [{ loader: 'babel-loader'}]
+      },
+      {
+        test: /\.s(a|c)ss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
+}
